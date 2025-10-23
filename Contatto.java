e8b2a0a49c40004aafbf97f685e6ff10a072cb36
domenicoromano1773-pixel <domenicoromano1773@gmail.com>
2025-10-23 09:02:30 +0200

    Refactor Contatto class to use String for NumeroTel

--- Contatto.java
+++ Contatto.java
@@ -1,11 +1,14 @@
 
 
+package Rubrica;
+
+
 public class Contatto {
 	private String Nome ;
-	private int  NumeroTel;
+	private	String  NumeroTel;
 	private String email ;
 	
-	Contatto(String Nome ,int NumeroTel,String email ){
+	Contatto(String Nome ,String NumeroTel,String email ){
 		this.Nome = Nome;
 		this.email = email;
 		this.NumeroTel = NumeroTel;
@@ -29,11 +32,11 @@ public class Contatto {
 		this.email = e;
 	}
 	
-	public int getNumero() {
+	public String getNumero() {
 		return  NumeroTel; 
 		
 	}
-	public void SetNumero(int NumeroTel) {
+	public void SetNumero(String NumeroTel) {
 		this.NumeroTel = NumeroTel; 
 		
 	}
