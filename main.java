1faaf357e3e6fcdf1031ed38dfc159342767d5e8
domenicoromano1773-pixel <domenicoromano1773@gmail.com>
2025-10-23 09:02:00 +0200

    Refactor main method to launch RubricaGUI

--- main.java
+++ main.java
@@ -3,9 +3,12 @@ import javax.swing.SwingUtilities;
 
 public class main {
 	public static void main(String[] args) {
-		Contatto contatto1 = new Contatto("Domenico",351168806,"rdomenico30@yahoo.com");
-		Contatto contatto2 = new Contatto("Gianluca", 2222222,"gianluca@gmail.com");
-		Contatto contatto3 = new Contatto("Gianluca", 2222222,"gianluca@gmail.com");
-	       
+	 SwingUtilities.invokeLater(new Runnable() {
+	            public void run() {
+	                RubricaGUI gui = new RubricaGUI();
+	                gui.setVisible(true);
+	            }
+	        });  
 	}
+
 }
